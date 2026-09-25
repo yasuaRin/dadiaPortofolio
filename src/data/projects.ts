417725befdfc2694f35e13f6a3e013deaa94be04
yasuaRin <dadiaputu@gmail.com>
2026-09-25 21:46:01 +0800

    changing gdrive url.4

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,7 +4,7 @@ import salesImg from '../assets/images/sales.png';
 import sosproImg from '../assets/images/sospro.png';
 import gpaTrackerImg from '../assets/images/gpaTracker.png';
 import teravinceImg from '../assets/images/teravince.jpg';
-import portfolioImg from '../assets/images/portfolio.png';
+import portfolioImg from '../assets/images/portofolio.png';
 
 export const projectsData: Project[] = [
   {
